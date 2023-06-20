466fe4104f930d8561f399688506535ea8ae53ff
kennycallado <kennycallado@gmail.com>
2023-06-20 13:20:45 +0200

    Initial commit

---
+++ src/app/providers/models/mod.rs
@@ -0,0 +1,8 @@
+pub mod answer;
+pub mod fcm;
+pub mod paper;
+pub mod project;
+pub mod question;
+pub mod resource;
+pub mod record;
+pub mod slide;
---
+++ src/app/server.rs
@@ -0,0 +1,46 @@
+#[cfg(feature = "cron")]
+use crate::app::providers::services::cron::CronManager;
+
+#[cfg(feature = "db")]
+use crate::database::connection;
+#[cfg(feature = "db")]
+use rocket::fairing::AdHoc;
+
+#[cfg(feature = "fetch")]
+use crate::app::providers::services::fetch::Fetch;
+
+use crate::app::providers::cors;
+
+use super::modules::routing as modules_routing;
+use super::routing as service_routing;
+
+#[launch]
+pub async fn rocket() -> _ {
+    #[allow(unused_mut)]
+    let mut rocket_build = rocket::build();
+
+    #[cfg(feature = "db")]
+    {
+        rocket_build = rocket_build
+            .attach(connection::Db::fairing())
+            .attach(AdHoc::on_ignite(
+                "Diesel Migrations",
+                connection::run_migrations,
+            ));
+    }
+
+    #[cfg(feature = "fetch")]
+    {
+        rocket_build = rocket_build.manage(Fetch::new());
+    }
+
+    #[cfg(feature = "cron")]
+    {
+        rocket_build = rocket_build.manage(CronManager::new().await);
+    }
+
+    rocket_build
+        .attach(cors::Cors)
+        .attach(service_routing::router())
+        .attach(modules_routing::router())
+}
