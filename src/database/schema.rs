021256ce9feb67d2063df67073ca78a3f676ceac
kennycallado <kennycallado@hotmail.com>
2023-07-25 17:58:41 +0200

    ADD Agrega migraciones iniciales

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,1 +1,13 @@
 // @generated automatically by Diesel CLI.
+
+diesel::table! {
+    cronjobs (id) {
+        id -> Uuid,
+        schedule -> Varchar,
+        service -> Varchar,
+        status -> Varchar,
+        route -> Varchar,
+        since -> Nullable<Timestamptz>,
+        until -> Nullable<Timestamptz>,
+    }
+}
