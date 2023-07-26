1e17f4f8883e09ba750c845671d7460aaad17e56
kennycallado <kennycallado@hotmail.com>
2023-07-26 18:11:46 +0200

    UPDATE Actualiza toda la lógica del módulo cron para trabajar correctamente con el servicio

--- src/app/modules/cron/controller.rs
+++ src/app/modules/cron/controller.rs
@@ -7,36 +7,18 @@ use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
 
 use crate::app::providers::config_getter::ConfigGetter;
 use crate::app::providers::guards::claims::AccessClaims;
+use crate::app::providers::models::cronjob::{PubNewCronJob, PubCronJob};
 use crate::app::providers::services::claims::{Claims, UserInClaims};
 use crate::app::providers::services::cron::CronManager;
-use crate::app::providers::services::cron::CronJob as CronJobTrait;
 
 use crate::app::modules::cron::model::{CronJob, NewCronJob};
 
 pub fn routes() -> Vec<rocket::Route> {
     routes![
-        hello,
-        hello_satete,
-        add,
         index,
+        add,
+        delete,
     ]
-}
-
-#[get("/hello")]
-pub fn hello() -> &'static str {
-    "Hello from cron"
-}
-
-#[get("/hello/state")]
-pub async fn hello_satete() -> String {
-    let res = reqwest::get("http://localhost:8000/api/v1/state/hello").await.unwrap();
-
-    match res.text().await {
-        Ok(text) => {
-            text.to_owned()
-        },
-        Err(_) => "error".to_owned()
-    }
 }
 
 #[post("/", data = "<new_cronjob>")]
@@ -51,48 +33,131 @@ pub async fn add(cron: &State<CronManager>, claims: AccessClaims, new_cronjob: J
 }
 
 async fn helper(cron: &State<CronManager>, _user: UserInClaims, new_cronjob: NewCronJob) -> Result<Json<Uuid>, Status> {
-    let new_cronjob: NewCronJob = new_cronjob;
-    let service = ConfigGetter::get_entity_url(new_cronjob.service.as_str()).unwrap();
+    let new_cronjob: NewCronJob = new_cronjob; // ???
+    // let route = new_cronjob.route.clone();
+    // let service = ConfigGetter::get_entity_url(new_cronjob.service.as_str()).unwrap();
+    // let manager: CronManager = cron.inner().clone();
 
-    let job = Job::new_async(new_cronjob.schedule.as_str(), move |_uuid, _l|  {
-        let url = format!("{}{}", service, new_cronjob.route);
-        Box::pin(async move {
-            let robo_token = Claims::from(UserInClaims::default()).enconde_for_robot();
+    let new_cronjob = PubNewCronJob {
+        route: new_cronjob.route,
+        schedule: new_cronjob.schedule,
+        service: new_cronjob.service,
+        status: "pending".to_owned(),
+        until: new_cronjob.until,
+        since: new_cronjob.since,
+    };
 
-            let res;
-            {
-                let client = reqwest::Client::new();
-                res = client
-                    .get(url)
-                    .bearer_auth(robo_token.unwrap())
-                    .header("Accept", "application/json")
-                    .send().await.unwrap();
-            }
-
-            if let Err(e) = res.error_for_status_ref() {
-                println!("Error: {}", e);
-                println!("Maybe there is a way to inform and kill its own self")
-            }
-        })
-    });
-
-    match job {
-        Ok(job) => {
-            let id = job.guid();
-            cron.add_job(job).await.unwrap();
-
-            Ok(Json(id))
-        },
-        Err(_) => Err(Status::InternalServerError),
+    match cron.create_job(&new_cronjob).await {
+        Ok(id) => Ok(Json(id)),
+        Err(e) => {
+            println!("Error: {};", e);
+            Err(Status::InternalServerError)
+        }
     }
+
+    // all inside the closure is executed every time
+    // let job = Job::new_async(new_cronjob.schedule.as_str(), move |uuid, mut lock|  {
+    //     let url = format!("{}{}", service, route);
+    //     let manager = manager.clone();
+
+    //     Box::pin(async move {
+    //         let job = manager.get_job(uuid).await.unwrap();
+    //         let next_tick = lock.next_tick_for_job(uuid).await.unwrap().unwrap().naive_utc();
+    //         let until = job.until.clone();
+
+    //         // println!("Job {} executed at {:?}", uuid, next_tick);
+    //         // println!("Until: {:?}", until);
+    //         // println!("Difference: {:?}", until - next_tick);
+
+    //         // first layer to match state of job
+    //         // if job is finished or error, remove from scheduler
+    //         // if job is active, do nothing
+    //         // if job is pending, execute
+    //         // this way we can handle the state of the job
+    //         // and remove it from the scheduler when it's finished
+    //         // or when it's failed
+
+    //         // println!("Status: {}", job.status);
+
+    //         manager.status_hanler(&mut lock, job, next_tick).await;
+
+    //         if let Some(until) = until {
+    //             if until < next_tick {
+    //                 // change state to finished
+    //                 manager.update_status(uuid, "finished").await.unwrap();
+    //                 // lock.remove(&uuid).await.unwrap();
+
+    //                 return ;
+    //             }
+    //         }
+
+    //         let robo_token = Claims::from(UserInClaims::default()).enconde_for_robot();
+    //         let res;
+    //         {
+    //             let client = reqwest::Client::new();
+    //             res = client
+    //                 .get(url)
+    //                 .bearer_auth(robo_token.unwrap())
+    //                 .header("Accept", "application/json")
+    //                 .send().await;
+    //         }
+
+    //         if let Err(e) = res {
+    //             println!("Error: {};", e);
+
+    //             // change the state to error
+    //             manager.update_status(uuid, "error").await.unwrap();
+    //             // lock.remove(&uuid).await.unwrap();
+    //        }
+    //     })
+    // });
+
+    // Only executed on the first time
+    // match job {
+    //     Ok(job) => {
+    //         let id = job.guid();
+
+    //         let cron_job = PubNewCronJob {
+    //             schedule: new_cronjob.schedule,
+    //             service: new_cronjob.service,
+    //             route: new_cronjob.route,
+    //             since: new_cronjob.since,
+    //             until: new_cronjob.until,
+    //         };
+
+    //         cron.add_job(job, cron_job).await.unwrap();
+
+    //         Ok(Json(id))
+    //     },
+    //     Err(e) => {
+    //         println!("Error: {};", e);
+    //         Err(Status::InternalServerError)
+    //     },
+    // }
 }
 
 #[get("/")]
-pub async fn index(cron: &State<CronManager>, claims: AccessClaims) -> Result<Json<Vec<CronJobTrait>>, Status> {
+pub async fn index(cron: &State<CronManager>, claims: AccessClaims) -> Result<Json<Vec<PubCronJob>>, Status> {
     match claims.0.user.role.name.as_str() {
         "admin" => { let jobs = cron.get_jobs().await; Ok(Json(jobs)) },
         _ => {
             println!("Error: index; Role not handled");
+            Err(Status::Unauthorized)
+        }
+    }
+}
+
+#[delete("/<id>")]
+pub async fn delete(cron: &State<CronManager>, claims: AccessClaims, id: Uuid) -> Result<Status, Status> {
+    match claims.0.user.role.name.as_str() {
+        "admin" => {
+            match cron.remove_job(id).await {
+                Ok(_) => Ok(Status::Ok),
+                Err(_) => Err(Status::InternalServerError),
+            }
+        },
+        _ => {
+            println!("Error: delete; Role not handled");
             Err(Status::Unauthorized)
         }
     }
--- src/app/modules/cron/model.rs
+++ src/app/modules/cron/model.rs
@@ -1,12 +1,19 @@
+use chrono::{DateTime, Utc, NaiveDateTime};
 use rocket::serde::uuid::Uuid;
 use serde::{Deserialize, Serialize};
+
+use crate::app::providers::models::cronjob::PubCronJob;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct CronJob {
     pub id: Uuid,
     pub schedule: String,
+    pub service: String,
+    pub status: String,
     pub route: String,
+    pub since: Option<NaiveDateTime>,
+    pub until: Option<NaiveDateTime>
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -15,4 +22,6 @@ pub struct NewCronJob {
     pub schedule: String,
     pub service: String,
     pub route: String,
+    pub since: Option<DateTime<Utc>>,
+    pub until: Option<DateTime<Utc>>
 }
