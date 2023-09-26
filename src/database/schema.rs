cc5990400c03c51fcc9dfd6928f466a9539b9ebf
kennycallado <kennycallado@hotmail.com>
2023-09-26 15:36:25 +0200

    UPDATE Acutaliza migraciones para descoplar escalon-jobs

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -2,12 +2,27 @@
 
 diesel::table! {
     cronjobs (id) {
-        id -> Uuid,
-        schedule -> Varchar,
+        id -> Int4,
+        owner -> Varchar,
         service -> Varchar,
-        status -> Varchar,
         route -> Varchar,
+        job_id -> Uuid,
+    }
+}
+
+diesel::table! {
+    escalonjobs (id) {
+        id -> Uuid,
+        status -> Varchar,
+        schedule -> Varchar,
         since -> Nullable<Timestamptz>,
         until -> Nullable<Timestamptz>,
     }
 }
+
+diesel::joinable!(cronjobs -> escalonjobs (job_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    cronjobs,
+    escalonjobs,
+);
