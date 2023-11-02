233d2ca224e78a052bcdacf13211168fee3a6159
kennycallado <kennycallado@hotmail.com>
2023-11-02 16:13:44 +0100

    UPDATE Actualiza migraciones para sqlx

--- src/database/migrations/20231102150908_create_escalonjobs.up.sql
+++ src/database/migrations/20231102150908_create_escalonjobs.up.sql
@@ -5,7 +5,9 @@ ALTER TABLE escalonjobs
   ADD COLUMN status   VARCHAR(20) NOT NULL,
   ADD COLUMN schedule VARCHAR(20) NOT NULL,
   ADD COLUMN since    TIMESTAMP WITH TIME ZONE,
-  ADD COLUMN until    TIMESTAMP WITH TIME ZONE;
+  ADD COLUMN until    TIMESTAMP WITH TIME ZONE
+  ;
 
 ALTER TABLE escalonjobs ADD CONSTRAINT check_escalonjobs_state CHECK
-  (status IN ('scheduled', 'running', 'done', 'failed'));
+  (status IN ('scheduled', 'running', 'done', 'failed'))
+  ;
